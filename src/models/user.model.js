2e2ed07755e10ec4e3116f0efbbc7230b0b935d8
Esma <esmanurgkky10@gmail.com>
2025-10-10 16:15:09 +0300

    feat: implement user model with schema definition

---
+++ src/models/user.model.js
@@ -0,0 +1,28 @@
+import mongose from 'mongoose';
+
+const userSchema = new mongose.Schema({
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+    fullName: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        mminlength: 6,
+    },
+    profilePic:{
+        type: String,
+        default: "",
+    },
+}, 
+    { timestamps: true }
+);
+
+const User = mongose.model('User', userSchema);
+
+export default User;
