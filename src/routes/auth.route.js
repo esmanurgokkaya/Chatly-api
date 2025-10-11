3b12193ad848c6106cc276c7931a105e8c1aa90f
Esma <esmanurgkky10@gmail.com>
2025-10-12 02:22:44 +0300

    feat: add check route to return authenticated user information

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { signup, login, logout, updateProfile } from "../controllers/auth.controller.js";
-import { protectRoute } from "../middlewares/auth.middleware.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
@@ -11,5 +11,9 @@ router.post("/login", login);
 router.post("/logout", logout);
 
 router.post("/update-profile", protectRoute, updateProfile);
+
+router.get("/check", protectRoute, (req, res) => {
+  res.status(200).json(req.user);
+});
 
 export default router;
